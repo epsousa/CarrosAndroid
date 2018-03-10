b85e43fc997f2b77ccf4003daacfe2097f870cec
LAPTOP-8KBMGR26\evair <evairpsousa@gmail.com>
2018-03-10 16:28:04 -0300

    .

---
+++ app/src/main/java/com/example/evair/carros/ui/listacarros/ListaCarrosAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.evair.carros.ui.listacarros
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+
+class ListaCarrosAdapter(private val carros: List<Carro>) : RecyclerView.Adapter<ListaCarrosAdapter.MeuViewHolder> {
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: MeuViewHolder?, position: Int) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MeuViewHolder {
+
+    }
+
+    class MeuViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
