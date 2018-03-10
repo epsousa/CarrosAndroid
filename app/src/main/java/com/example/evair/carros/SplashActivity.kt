ae16473154b97707eb331cf433ee40d398ba4f29
LAPTOP-8KBMGR26\evair <evairpsousa@gmail.com>
2018-03-10 09:41:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/evair/carros/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.evair.carros
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        carregar()
+    }
+
+    fun carregar() {
+        val animacao = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
+
+        ivLogoSplash.startAnimation(animacao)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            this.finish()
+        }, 3000)
+    }
+}
